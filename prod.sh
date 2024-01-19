a49cc13dc5082c9da290cb164bc7da61884ec118
Keith Hollis <keith.hollis@gmail.com>
2024-01-19 12:25:31 +0000

    master_tag Added a prod service

---
+++ prod.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+export LC_ALL=en_US
+
+SCRIPTPATH=`dirname "$0"`
+
+cd $SCRIPTPATH
+
+source venv/bin/activate
+
+#gunicorn --workers 1 --threads 5 --timeout 0 --bind :6050 wsgi:app
+
+while true
+do
+  python harness.py -g10 || exit 1
+  sleep 1
+done
--- stats.py
+++ stats.py
@@ -5,10 +5,16 @@ from sqlalchemy import and_, or_
 from database import db_init, HEARTS_DB_URI
 from model import GameModel, PlayerModel
 
+MIN_NUM_GAMES = 5
+
 logger = logging.getLogger()
 logging.basicConfig(filename="/var/log/hearts/log", level=logging.DEBUG)
 
 session = db_init(HEARTS_DB_URI)
+
+stats = []
+
+num_games = session.query(GameModel).filter(GameModel.finish.isnot(None)).count()
 
 players = session.query(PlayerModel).all()
 for player in players:
@@ -24,6 +30,8 @@ for player in players:
         )
         .count()
     )
+    if games_played < MIN_NUM_GAMES:
+        continue
     games_won = (
         session.query(GameModel)
         .filter(
@@ -36,6 +44,11 @@ for player in players:
         )
         .count()
     )
+    stats.append((player, games_played, games_won, games_won * 100 / games_played))
 
-    if games_played:
-        print(f"player: {games_won*100/games_played:.0f}% played {games_played} won {games_won} {player.name}")
+stats.sort(key=lambda x: x[3], reverse=True)
+
+print(f"number of games: {num_games}")
+print("")
+for stat in stats:
+    print(f"{stat[3]:2.0f}% played {stat[1]} won {stat[2]} player: {stat[0].id} {stat[0].name}")
